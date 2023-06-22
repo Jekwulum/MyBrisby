22c4e71a9f744c750fbfda3c7d6ce8c781b17426
Charles <charlesnwoye2@gmail.com>
2023-06-22 07:13:33 +0100

    increasing and decreasing passengers for pickup page

--- js/pickup_script.js
+++ js/pickup_script.js
@@ -1,6 +1,9 @@
 let passengersDisplay = document.querySelector(".passengers-display");
 let totalPassengers = document.querySelector(".total-passengers");
 let passengersDiv = document.querySelector(".passengers-div");
+let adultsFigure = document.querySelector('.adults-figure');
+let childrenFigure = document.querySelector('.children-figure');
+let infantsFigure = document.querySelector('.infants-figure');
 
 
 passengersDisplay.addEventListener("click", () => {
@@ -10,3 +13,55 @@ passengersDisplay.addEventListener("click", () => {
     passengersDiv.style.display = 'none';
   }
 });
+
+let passengersObj = { infants: 0, children: 0, adults: 1 };
+let passengers = Object.values(passengersObj).reduce((acc, curr) => acc + curr, 0);
+totalPassengers.textContent = passengers;
+adultsFigure.textContent = passengersObj["adults"];
+childrenFigure.textContent = passengersObj["children"];
+infantsFigure.textContent = passengersObj["infants"];
+
+
+function increasePassengers(category) {
+  if (category === "adults") {
+    {
+      passengersObj["adults"] += 1;
+      adultsFigure.textContent = passengersObj["adults"];
+    }
+  } else if (category === "children") {
+    {
+      passengersObj["children"] += 1;
+      childrenFigure.textContent = passengersObj["children"];
+    }
+  } else if (category === "infants") {
+    {
+      passengersObj["infants"] += 1;
+      infantsFigure.textContent = passengersObj["infants"];
+    }
+  }
+
+  passengers = Object.values(passengersObj).reduce((acc, curr) => acc + curr, 0);
+  totalPassengers.textContent = passengers;
+};
+
+function decreasePassengers(category) {
+  if (category === "adults") {
+    {
+      passengersObj["adults"] == 0 ? "" : passengersObj["adults"] -= 1;
+      adultsFigure.textContent = passengersObj["adults"];
+    }
+  } else if (category === "children") {
+    {
+      passengersObj["children"] == 0 ? "" : passengersObj["children"] -= 1;
+      childrenFigure.textContent = passengersObj["children"];
+    }
+  } else if (category === "infants") {
+    {
+      passengersObj["infants"] == 0 ? "" : passengersObj["infants"] -= 1;
+      infantsFigure.textContent = passengersObj["infants"];
+    }
+  }
+
+  passengers = Object.values(passengersObj).reduce((acc, curr) => acc + curr, 0);
+  totalPassengers.textContent = passengers;
+};
