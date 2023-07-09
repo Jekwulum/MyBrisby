fec88ab53f84aa9ae9644a111fefe00a3c2674ba
Charles <charlesnwoye2@gmail.com>
2023-07-09 19:53:14 +0100

    dashboard setup

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,7 +1,22 @@
 window.addEventListener("DOMContentLoaded", () => {
   let token = sessionStorage.getItem("token");
 
-  if (!token) {
-    window.location.href = 'index.html';
-  }
+  // if (!token) {
+  //   window.location.href = 'index.html';
+  // }
 });
+
+function toggleIcon(iconId, divClass) {
+  let icon = document.getElementById(iconId);
+  let div = document.getElementById(divClass);
+
+  if (icon.classList.contains('fa-circle-chevron-down')) {
+    div.style.height = '90px';
+    icon.classList.remove('fa-circle-chevron-down');
+    icon.classList.add('fa-circle-chevron-up');
+  } else {
+    div.style.height = 'auto';
+    icon.classList.remove('fa-circle-chevron-up');
+    icon.classList.add('fa-circle-chevron-down');
+  }
+};
